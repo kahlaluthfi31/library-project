b8285ff627ace28d680275a7cf4885d2453c7756
kahlaluthfi31 <kahlaluthifiyah@gmail.com>
2026-04-22 02:24:56 +0700

    Fix app bootstrap for vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,8 +6,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -17,4 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->registered(function ($app) {
+        if (isset($_SERVER['VERCEL']) || env('VERCEL')) {
+            $app->useStoragePath('/tmp/storage');
+            $app->setBootstrapCachePath('/tmp/storage/bootstrap/cache');
+        }
+    })
+    ->create();
