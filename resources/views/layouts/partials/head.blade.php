927a9bbf879392657ea1e7d1e578b44960bb6718
kahlaluthfi31 <kahlaluthifiyah@gmail.com>
2026-04-22 20:35:51 +0700

    perbaikan layoyts

---
+++ resources/views/layouts/partials/head.blade.php
@@ -0,0 +1,4 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>{{ $title ?? config('app.name', 'AmartaLib') }}</title>
+@vite(['resources/css/app.css', 'resources/js/app.js'])
