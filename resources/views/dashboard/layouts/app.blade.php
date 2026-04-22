186f2be91058339687ba010792aa7f5582263fec
kahlaluthfi31 <kahlaluthifiyah@gmail.com>
2026-04-22 16:49:18 +0700

    perbaikan vite

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Dashboard' }} - AmartaLib</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="h-screen bg-slate-50 text-slate-800 overflow-hidden">
